dbf090d8676da0b483987b90b0ad512ee5e759f6
Ahmad Niam <ahmadniam@Ahmads-MacBook-Air.local>
2026-06-04 12:38:10 +0700

    Simplify modelling script for MLflow project

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,24 +6,16 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import train_test_split
 
-# Set MLflow Tracking URI Github Actions
-mlflow.set_tracking_uri("file:./mlruns")
 
-# Nama experiment
-mlflow.set_experiment("Telco Customer Churn Basic Model")
-
-# Mengaktifkan autolog sesuai kriteria basic
+# Tidak perlu set_tracking_uri dan set_experiment saat berjalan di MLflow Project
 mlflow.sklearn.autolog()
 
-# Load dataset hasil preprocessing
 data_path = "telco_preprocessing/telco_customer_churn_preprocessed.csv"
 df = pd.read_csv(data_path)
 
-# Memisahkan fitur dan target
 X = df.drop(columns=["Churn"])
 y = df["Churn"]
 
-# Split data
 X_train, X_test, y_train, y_test = train_test_split(
     X,
     y,
@@ -32,26 +24,27 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=y
 )
 
-# Membuat dan melatih model
 model = RandomForestClassifier(
     n_estimators=100,
     random_state=42
 )
 
-with mlflow.start_run(run_name="RandomForest_Basic_Autolog"):
-    model.fit(X_train, y_train)
+model.fit(X_train, y_train)
 
-    # Prediksi
-    y_pred = model.predict(X_test)
+y_pred = model.predict(X_test)
 
-    # Evaluasi tambahan
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
 
-    print("Training selesai.")
-    print(f"Accuracy : {accuracy:.4f}")
-    print(f"Precision: {precision:.4f}")
-    print(f"Recall   : {recall:.4f}")
-    print(f"F1-score : {f1:.4f}")
+mlflow.log_metric("accuracy", accuracy)
+mlflow.log_metric("precision", precision)
+mlflow.log_metric("recall", recall)
+mlflow.log_metric("f1_score", f1)
+
+print("Training selesai.")
+print(f"Accuracy : {accuracy:.4f}")
+print(f"Precision: {precision:.4f}")
+print(f"Recall   : {recall:.4f}")
+print(f"F1-score : {f1:.4f}")
